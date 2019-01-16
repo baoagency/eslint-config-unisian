d485302fe1ab5c32dfe79c9b52539e24406edb19
Dan Gamble <dan@dangamble.co.uk>
2019-01-16 15:19:53 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: [
+    'standard',
+    'plugin:compat/recommended'
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+    allowImportExportEverywhere: true
+  },
+  plugins: ['compat', 'html', 'prettier'],
+  env: {
+    browser: true,
+    es6: true
+  },
+  rules: {
+    'comma-dangle': ['error', 'only-multiline'],
+    'no-new': ['error'],
+    'no-var': ['error'],
+    'object-shorthand': ['error', 'always'],
+    'prefer-arrow-callback': ['error', { allowNamedFunctions: true }],
+    'prefer-const': ['error'],
+    'prefer-template': ['error'],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-unused-vars': ['error']
+  },
+  globals: {
+    document: true,
+    window: true
+  }
+}

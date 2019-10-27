3ddc2f32749de337f4e1b7af1874b2e9e73b6bed
Dan Gamble <dan@dangamble.co.uk>
2019-10-27 12:25:39 +0000

    Add eslint-plugin-import and eslint-plugin-react

--- index.js
+++ index.js
@@ -1,30 +1,48 @@
 module.exports = {
   extends: [
     'standard',
-    'plugin:compat/recommended',
     'prettier',
     'prettier/standard',
+    'plugin:compat/recommended',
+    'plugin:react/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
   ],
   parser: 'babel-eslint',
   parserOptions: {
     sourceType: 'module',
     allowImportExportEverywhere: true
   },
-  plugins: ['compat', 'html', 'prettier'],
+  plugins: ['compat', 'html', 'prettier', 'react', 'import'],
   env: {
     browser: true,
     es6: true
   },
   rules: {
-    'comma-dangle': ['error', 'only-multiline'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-console': ['error', { allow: ['error', 'info', 'warn'] }],
     'no-new': ['error'],
+    'no-unused-vars': ['error'],
     'no-var': ['error'],
     'object-shorthand': ['error', 'always'],
     'prefer-arrow-callback': ['error', { allowNamedFunctions: true }],
     'prefer-const': ['error'],
     'prefer-template': ['error'],
-    'no-console': ['error', { allow: ['error', 'info', 'warn'] }],
-    'no-unused-vars': ['error']
+
+    // React
+    'jsx-quotes': ['error', 'prefer-double'],
+
+    // Import
+    'import/no-default-export': ['error'],
+    'import/order': ['error', {
+      'groups': [
+        'builtin',
+        'external',
+        ['internal', 'parent'],
+        'sibling',
+      ],
+      'newlines-between': 'always'
+    }],
   },
   globals: {
     document: true,
